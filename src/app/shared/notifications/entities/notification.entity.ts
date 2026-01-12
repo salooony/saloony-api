dfb1eb182b4c9411b52d93748a4241a7942f4a6a
mohammed4122002 <pombut777@gmail.com>
2026-01-12 09:32:53 +0200

    feature/notification-entity-39

---
+++ src/app/shared/notifications/entities/notification.entity.ts
@@ -0,0 +1,63 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    ManyToOne,
+    JoinColumn,
+} from 'typeorm';
+import { User } from '../../../user/infrastructure/schemas/user.entity';
+import { NotificationType, NotificationStatus, NotificationTemplateKey } from '../enums/notification-type.enum';
+
+
+@Entity('notifications')
+export class Notification {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    recipientId: string;
+
+    @ManyToOne(() => User, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'recipientId' })
+    recipient: User;
+
+    @Column({
+        type: 'enum',
+        enum: NotificationTemplateKey,
+        nullable: true,
+    })
+    templateKey: NotificationTemplateKey;
+
+    @Column({ type: 'jsonb', nullable: true })
+    parameters: Record<string, any>;
+
+
+    @Column({ type: 'varchar', length: 255 })
+    title: string;
+
+
+    @Column('text')
+    message: string;
+
+    @Column({
+        type: 'enum',
+        enum: NotificationType,
+        default: NotificationType.SYSTEM,
+    })
+    type: NotificationType;
+
+    @Column({
+        type: 'enum',
+        enum: NotificationStatus,
+        default: NotificationStatus.UNREAD,
+    })
+    status: NotificationStatus;
+
+
+    @Column({ type: 'jsonb', nullable: true })
+    metadata: Record<string, any>;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt: Date;
+}
