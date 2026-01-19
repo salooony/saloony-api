8eea61a36d6d8108a07ec599f71ddffbe85ecca3
Your Name <41466655+Sajaalghoul@users.noreply.github.com>
2026-01-19 12:19:44 +0200

    schemas created

---
+++ src/app/address/infrastructure/schemas/city.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Country } from './country.entity';
+
+@Entity({ name: 'city' })
+export class City {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @ManyToOne(() => Country, { nullable: false })
+  @JoinColumn({ name: 'country_id' })
+  country: Country;
+}
