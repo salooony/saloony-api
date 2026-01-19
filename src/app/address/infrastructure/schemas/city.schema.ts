c96a92d67d3b4a32ec2842df49b5b843aebbb92f
Your Name <41466655+Sajaalghoul@users.noreply.github.com>
2026-01-19 12:57:10 +0200

    schemas created

--- src/app/address/infrastructure/schemas/city.schema.ts
+++ src/app/address/infrastructure/schemas/city.schema.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { Country } from './country.entity';
+import { Country } from './country.schema';
 
 @Entity({ name: 'city' })
 export class City {
