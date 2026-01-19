8814908928e6bfd07b4e6bd773f810fbcb4de8c7
Your Name <41466655+Sajaalghoul@users.noreply.github.com>
2026-01-19 13:53:00 +0200

    creates the address enetity and schema

---
+++ src/app/address/infrastructure/schemas/address.schema.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { City } from './city.schema';
+
+@Entity({ name: 'address' })
+export class Address {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'jsonb' })
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  @Column({ type: 'varchar', length: 10 })
+  postcode: string;
+
+  @Column({ type: 'uuid', name: 'city_id' })
+  cityId: string;
+
+  @ManyToOne(() => City, { nullable: false })
+  @JoinColumn({ name: 'city_id' })
+  city: City;
+
+  @Column({ type: 'varchar', length: 255 })
+  address: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  complement?: string;
+}
