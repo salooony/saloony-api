8bf28cbb6fbc621dfc0da0b9539e00760b3bbfe7
Rozana <rozanashaqqou74@gmail.com>
2026-03-15 11:27:27 +0200

    fixed import Country in City port

--- src/app/address/domain/ports/icity.repository.ts
+++ src/app/address/domain/ports/icity.repository.ts
@@ -1,6 +1,7 @@
-export const CITY_REPOSITORY = 'CITY_REPOSITORY';
-
 import { City } from '../entities/city.entity';
+import { Country } from '../entities/country.entity';
+
+export const CITY_REPOSITORY = 'CITY_REPOSITORY';
 
 export interface ICityRepository {
   save(city: City): Promise<City>;
