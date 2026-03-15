ab10c373e43974bce957869f767c1ad4b8838803
Rozana <rozanashaqqou74@gmail.com>
2026-03-15 11:20:00 +0200

    fix format

--- src/app/address/domain/ports/icity.repository.ts
+++ src/app/address/domain/ports/icity.repository.ts
@@ -5,5 +5,5 @@ import { City } from '../entities/city.entity';
 export interface ICityRepository {
   save(city: City): Promise<City>;
 
-    findCountryById(id: string): Promise<Country | null>;
+  findCountryById(id: string): Promise<Country | null>;
 }
