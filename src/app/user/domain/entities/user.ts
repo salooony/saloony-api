302281f1da1c98193724c86763ecd24cc73e1548
Moha A AL J <135424927+MohaAAlJ@users.noreply.github.com>
2026-03-17 15:25:23 +0200

    refactor(user): update enums and defaults

--- src/app/user/domain/entities/user.ts
+++ src/app/user/domain/entities/user.ts
@@ -17,6 +17,7 @@ export class User {
   public acl: string[] = [];
   public deletedAt?: Date;
 
+  // Kept to avoid breaking unit tests that use a Mock Repository without schema support.
   public status: UserStatus = UserStatus.PENDING;
 
   public block(): void {
