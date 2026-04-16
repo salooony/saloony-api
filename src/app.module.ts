de111d0f3680da9638d8df504041db200385f2e9
Mohammed abu saada <125014007+mohammed4122002@users.noreply.github.com>
2026-04-16 12:32:23 +0300

    fix: update import paths for AuthorizationGuard and NotificationModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,8 @@ import jwtConfig from '@config/jwt.config';
 import { APP_GUARD } from '@nestjs/core';
 import { TokenGuard } from '@user/infrastructure/guards/token.guard';
 import { TokensModule } from '@token/infrastructure/modules/token.module';
-import { AuthorizationGuard } from '@app/shared/guards/authorization.guard';
-import { NotificationModule } from '@app/notification';
+import { AuthorizationGuard } from '@shared/guards/authorization.guard';
+import { NotificationModule } from '@notification';
 
 const ENV = process.env.NODE_ENV;
 
