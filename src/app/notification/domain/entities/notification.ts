9453f422a91bb5d273d1a5d78af61d7186752013
Mohammed abu saada <125014007+mohammed4122002@users.noreply.github.com>
2026-04-18 13:18:07 +0300

    feat: implement Notification entity schem and migration 39

---
+++ src/app/notification/domain/entities/notification.ts
@@ -0,0 +1,18 @@
+import { NotificationType } from '../enums/notification-type.enum';
+
+export class Notification {
+  public id: string;
+  public userId: string;
+  public type: NotificationType;
+  public title: string;
+  public content: string;
+  public isRead: boolean;
+  public readAt?: Date;
+  public createdAt: Date;
+  public updatedAt: Date;
+
+  public markAsRead(): void {
+    this.isRead = true;
+    this.readAt = new Date();
+  }
+}
