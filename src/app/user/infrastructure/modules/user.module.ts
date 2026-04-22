0ad2f36327bdc2e5caf49e53363d28e1a6d800c6
Rozana <rozanashaqqou74@gmail.com>
2026-04-22 12:42:56 +0300

    refactor: make some update

--- src/app/user/infrastructure/modules/user.module.ts
+++ src/app/user/infrastructure/modules/user.module.ts
@@ -24,6 +24,8 @@ import { UpdateAvatarUsecase } from '@user/application/usecases/update-avatar.us
 import { TokensModule } from '@token/infrastructure/modules/token.module';
 import { TemplateModule } from '@notification/infrastructure/modules/template.module';
 import { FileModule } from '@shared/uploads/infrastructure/modules/file.module';
+import { NotificationModule } from '@app/notification/infrastructure/modules/notification.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
